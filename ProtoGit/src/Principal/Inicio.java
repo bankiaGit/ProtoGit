4ca34ae9e3e37bb5a9d4ee2e0954d5b7ff2ebc99
A169800 <A169800@PCS41678.grupo.cm.es>
2013-08-30 13:15:29 +0200

    Creadas tareas intermedias.

--- ProtoGit/src/Principal/Inicio.java
+++ ProtoGit/src/Principal/Inicio.java
@@ -8,6 +8,7 @@ public class Inicio {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Iniciando proceso de arranque.");
+		System.out.println("Realizando tareas intermedias.");
 		System.out.println("Finalizando proceso de arranque.");
 
 	}
